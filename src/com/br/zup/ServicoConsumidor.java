fccfe4291c59a546cf30a9f6df1eff0026d3fba8
Vinícius <vinicius.raymundo@zup.com.br>
2021-02-18 14:21:40 -0300

    Adicionar metodo de cadastro para consumidor

--- src/com/br/zup/ServicoConsumidor.java
+++ src/com/br/zup/ServicoConsumidor.java
@@ -6,5 +6,20 @@ import java.util.List;
 public class ServicoConsumidor {
     private static List<Consumidor> consumidores = new ArrayList<>();
 
-    
+    public static void validarEmail(String email) throws Exception {
+        if(!email.contains("@")){
+            throw new Exception("Email invalido");
+        }
+        for(Consumidor consumidor : consumidores){
+            if(consumidor.getEmail().equals(email)){
+                throw new Exception("O Email já existe meu cumbade");
+            }
+        }
+    }
+    public static Consumidor cadastrarConsumidor(String nomeCompleto, String email) throws Exception {
+        validarEmail(email);
+        Consumidor consumidor = new Consumidor(nomeCompleto, email);
+        consumidores.add(consumidor);
+        return consumidor;
+    }
 }
