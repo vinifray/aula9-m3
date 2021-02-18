eeced24889c98250512a17301f68b6de94c60fab
Vinícius <vinicius.raymundo@zup.com.br>
2021-02-18 14:22:24 -0300

    Adicionar execução do sistema no metodo main

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -1,8 +1,17 @@
 package com.br.zup;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean continuar = true;
+        while (continuar){
+            try{
+                continuar = Sistema.executar();
+            }catch (Exception erro){
+                System.out.println(erro.getMessage());
+            }
+        }
     }
 }
