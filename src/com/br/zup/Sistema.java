cc68445290584844f183c5873205c9faf8bcc0ba
Vinícius <vinicius.raymundo@zup.com.br>
2021-02-18 14:22:05 -0300

    Adicionar opção de cadastro do consumidor

--- src/com/br/zup/Sistema.java
+++ src/com/br/zup/Sistema.java
@@ -7,7 +7,9 @@ public class Sistema {
     public static void menu(){
         StringBuilder model = new StringBuilder();
         model.append("Digite:\n");
-        model.append("1 para pesquisar por email do Consumidor");
+        model.append("-1 para sair\n");
+        model.append("1 para pesquisar por email do Consumidor\n");
+        model.append("2 para cadastrar consumidor\n");
         System.out.println(model.toString());
     }
 
@@ -18,7 +20,17 @@ public class Sistema {
         return fatura;
     }
 
-    public static void executar() throws Exception {
+    public static Consumidor cadastrarConsumidor() throws Exception {
+        System.out.println("Digite o nome completo");
+        String nomeCompleto = new Scanner(System.in).nextLine();
+
+        System.out.println("Digite o email");
+        String email = new Scanner(System.in).nextLine();
+
+        return ServicoConsumidor.cadastrarConsumidor(nomeCompleto, email);
+    }
+
+    public static boolean executar() throws Exception {
         boolean continuar = true;
         while (continuar){
             menu();
@@ -26,7 +38,11 @@ public class Sistema {
             if(resposta == 1){
                 Fatura fatura = pesquisarFatura();
                 System.out.println(fatura);
+            }else if(resposta == 2){
+                Consumidor consumidor = cadastrarConsumidor();
+                System.out.println(consumidor);
             }
         }
+        return continuar;
     }
 }
