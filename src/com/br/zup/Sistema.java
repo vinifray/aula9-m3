1e53564217db3ec4893a44d85e7920958be4488e
Vinícius <vinicius.raymundo@zup.com.br>
2021-02-18 13:53:39 -0300

    Criar metodo de menu para o Sistema

---
+++ src/com/br/zup/Sistema.java
@@ -0,0 +1,14 @@
+package com.br.zup;
+
+import java.util.Scanner;
+
+public class Sistema {
+
+    public static void menu(){
+        StringBuilder model = new StringBuilder();
+        model.append("Digite:\n");
+        model.append("1 para pesquisar por email do Consumidor");
+        System.out.println(model.toString());
+    }
+
+}
