bdf5ec4321c9f845f8a81bd56bed6424877b2fe6
Vinícius <vinicius.raymundo@zup.com.br>
2021-02-18 13:43:04 -0300

    Adicionar metodo de pesquisar por email do consumidor

--- src/com/br/zup/ServicoFatura.java
+++ src/com/br/zup/ServicoFatura.java
@@ -5,6 +5,14 @@ import java.util.List;
 
 public class ServicoFatura {
     private static List<Fatura> faturas = new ArrayList<>();
-    
+
+    public static Fatura pesquisarFaturaPeloEmailDoConsumidor(String email) throws Exception {
+        for (Fatura fatura : faturas){
+            if(fatura.getConsumidor().getEmail().equalsIgnoreCase(email)){
+                return fatura;
+            }
+        }
+        throw new Exception("Fatura não exite");
+    }
 
 }
