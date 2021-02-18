cf12cf1839926ced4d77b91edf7c770712a55e8b
Vinícius <vinicius.raymundo@zup.com.br>
2021-02-18 13:59:50 -0300

    Criar classe de Serviço para Consumidor

---
+++ src/com/br/zup/ServicoConsumidor.java
@@ -0,0 +1,10 @@
+package com.br.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicoConsumidor {
+    private static List<Consumidor> consumidores = new ArrayList<>();
+
+    
+}
